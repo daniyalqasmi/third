830c4fcda72b8ef657755fc8a0d45427b811a132
Muhammad Daniyal Qasmi <Daniyalqasmi1@gmail.com>
2023-06-14 21:28:56 +0500

    update

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -5,7 +5,7 @@ import via from "tailwindcss";
 
 export default function Hero() {
   return (
-    <div className="pl-24 bg-gradient-to-r from-purple-400  to-blue-500 via-95%" >
+    <div className=" bg-gradient-to-r from-purple-400  to-blue-500 via-95%" >
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
